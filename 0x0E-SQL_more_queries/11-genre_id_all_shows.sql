7a6228962cf4e605a93383a7a75f51834e951a66
Emmydapson <iloriemmanuel00@gmail.com>
2024-04-22 12:41:18 +0000

    task corrected

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
- -- This command Import the database dump of hbtn_0d_tvshows to your MySQL server: download (same as 10-genre_id_by_show.sql)
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-       LEFT JOIN `tv_show_genres` AS g
-       ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- lists all rows of tables in a database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
- -- This command Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 11-genre_id_all_shows.sql)
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-       LEFT JOIN `tv_show_genres` AS g
-       ON s.`id` = g.`show_id`
-       WHERE g.`genre_id` IS NULL
- ORDER BY s.`title`, g.`genre_id`;
+--  lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- lists all rows of a database that don't have one column
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
